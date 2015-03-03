e85437c56fe825bce3f9f3adf6ef2d86d117c48a
Suhoy95 <suhoy95@gmail.com>
2015-03-03 22:35:50 +0300

    отделение расчета статистики(контракты)

--- HomeTask/battleships/AiTester.cs
+++ HomeTask/battleships/AiTester.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using Ninject;
 using NLog;
 
@@ -12,6 +13,7 @@ namespace battleships
 	    private readonly IMapGenerator gen;
 	    private readonly IGameVisualizer vis;
 	    private readonly IProcessMonitor monitor;
+	    private DataOfStatistics data;
 	    private Func<string, Ai> aiFactory;
 	    private Func<Map, Ai, Game> gameFactory; 
 
@@ -19,38 +21,37 @@ namespace battleships
 
 	    public AiTester(Settings settings, IMapGenerator generator, 
                         IGameVisualizer visualizer, IProcessMonitor monitor,
+                        DataOfStatistics data,
                         Func<string, Ai> aiFactory, Func<Map, Ai, Game> gameFactory)
 	    {
             this.settings = settings;
             gen = generator;
             vis = visualizer;
             this.monitor = monitor;
+	        this.data = data;
 	        this.aiFactory = aiFactory;
 	        this.gameFactory = gameFactory;
 	    }
 
 		public void TestSingleFile(string exe)
 		{
-            var badShots = 0;
-            var crashes = 0;
-            var gamesPlayed = 0;
-            var shots = new List<int>();
             var ai = aiFactory(exe);
+		    data.name = ai.Name;
 
             for (var gameIndex = 0; gameIndex < settings.GamesCount; gameIndex++)
 			{
 			    var game = gameFactory(gen.GenerateMap(), ai);
 				RunGameToEnd(game);
-				gamesPlayed++;
-				badShots += game.BadShots;
+				data.gamesPlayed++;
+				data.badShots += game.BadShots;
 				if (game.AiCrashed)
 				{
-					crashes++;
-					if (crashes > settings.CrashLimit) break;
+					data.crashes++;
+					if (data.crashes > settings.CrashLimit) break;
                     ai = aiFactory(exe);
 				}
 				else
-					shots.Add(game.TurnsCount);
+					data.shots.Add(game.TurnsCount);
 			
                 if (settings.Verbose)
 				{
@@ -61,7 +62,7 @@ namespace battleships
 			}
 
 			ai.Dispose();
-			WriteTotal(ai, shots, crashes, badShots, gamesPlayed);
+			WriteTotal(SolveStatistics(data, settings));
 		}
 
 		private void RunGameToEnd(Game game)
@@ -79,19 +80,31 @@ namespace battleships
 			}
 		}
 
-		private void WriteTotal(Ai ai, List<int> shots, int crashes, int badShots, int gamesPlayed)
+	    private static Statistics SolveStatistics(DataOfStatistics d, Settings settings)
+	    {
+	        if (d.shots.Count == 0) d.shots.Add(1000*1000);
+	        d.shots.Sort();
+
+	        var result = new Statistics();
+	        result.name = d.name;
+	        result.crashes = d.crashes;
+	        result.gamesPlayed = d.gamesPlayed;
+	        result.median = d.shots.Count%2 == 1 ? d.shots[d.shots.Count/2]
+	                                             : (d.shots[d.shots.Count/2] + d.shots[(d.shots.Count + 1)/2])/2;
+	        result.mean = d.shots.Average();
+	        result.sigma = Math.Sqrt(d.shots.Average(s => (s - result.mean)*(s - result.mean)));
+	        result.badFraction = (100.0*d.badShots)/d.shots.Sum();
+	        result.crashPenalty = 100.0*d.crashes/settings.CrashLimit;
+	        result.efficiencyScore = 100.0*(settings.Width*settings.Height - result.mean)/(settings.Width*settings.Height);
+	        result.score = result.efficiencyScore - result.crashPenalty - result.badFraction;
+
+	        return result;
+	    }
+        
+        private static void WriteTotal(Statistics s)
 		{
-			if (shots.Count == 0) shots.Add(1000 * 1000);
-			shots.Sort();
-			var median = shots.Count % 2 == 1 ? shots[shots.Count / 2] : (shots[shots.Count / 2] + shots[(shots.Count + 1) / 2]) / 2;
-			var mean = shots.Average();
-			var sigma = Math.Sqrt(shots.Average(s => (s - mean) * (s - mean)));
-			var badFraction = (100.0 * badShots) / shots.Sum();
-			var crashPenalty = 100.0 * crashes / settings.CrashLimit;
-			var efficiencyScore = 100.0 * (settings.Width * settings.Height - mean) / (settings.Width * settings.Height);
-			var score = efficiencyScore - crashPenalty - badFraction;
 			var headers = FormatTableRow(new object[] { "AiName", "Mean", "Sigma", "Median", "Crashes", "Bad%", "Games", "Score" });
-			var message = FormatTableRow(new object[] { ai.Name, mean, sigma, median, crashes, badFraction, gamesPlayed, score });
+			var message = FormatTableRow(new object[] { s.name, s.mean, s.sigma, s.median, s.crashes, s.badFraction, s.gamesPlayed, s.score });
 			resultsLog.Info(message);
 			Console.WriteLine();
 			Console.WriteLine("Score statistics");
@@ -100,7 +113,7 @@ namespace battleships
 			Console.WriteLine(message);
 		}
 
-		private string FormatTableRow(object[] values)
+		private static string FormatTableRow(object[] values)
 		{
 			return FormatValue(values[0], 15) 
 				+ string.Join(" ", values.Skip(1).Select(v => FormatValue(v, 7)));
@@ -111,4 +124,27 @@ namespace battleships
 			return v.ToString().Replace("\t", " ").PadRight(width).Substring(0, width);
 		}
 	}
+
+    public class DataOfStatistics
+    {
+        public string name = "BaseDataStatistic name";
+        public long badShots = 0;
+        public long crashes = 0;
+        public long gamesPlayed = 0;
+        public List<int> shots = new List<int>();
+    }
+
+    public class Statistics
+    {
+        public string name;
+        public long crashes;
+        public long gamesPlayed;
+        public int median;
+        public double mean;
+        public double sigma;
+        public double badFraction;
+        public double crashPenalty;
+        public double efficiencyScore;
+        public double score;
+    }
 }
